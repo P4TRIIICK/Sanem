f494e725314b015d40e7ebb39f702a47daf8ad52
AristeuZH <aristeuzandomenighi@gmail.com>
2025-05-12 13:45:36 -0300

    reset password

---
+++ frontend/lib/screens/forgot_password_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ForgotPasswordPage extends StatelessWidget {
+  const ForgotPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Recuperar Senha")),
+      body: const Center(child: Text("Tela de Recuperação de Senha")),
+    );
+    
+  }
+}
