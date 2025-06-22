b4a85c531d914fdbd61eba082877d3c1ec75b3e0
Kauan Lopes <kndofc@hotmail.com>
2025-06-22 11:52:01 -0300

    base permissionamento

--- backend/app/Models/Pessoa.php
+++ backend/app/Models/Pessoa.php
@@ -5,13 +5,22 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles; // <-- 1. IMPORTE O TRAIT DO SPATIE
 
 class Pessoa extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    // 2. ADICIONE O HasRoles AQUI PARA GANHAR OS "SUPERPODERES"
+    use HasApiTokens, Notifiable, HasRoles;
 
     protected $table = 'pessoa';
     public $timestamps = false;
+
+    /**
+     * 3. Adicione esta linha para garantir que o Spatie funcione com Sanctum/API
+     * (Opcional se o seu guarda padrão for 'sanctum', mas é uma boa prática ser explícito)
+     */
+    protected $guard_name = 'sanctum';
+
 
     protected $fillable = [
         'nome',
